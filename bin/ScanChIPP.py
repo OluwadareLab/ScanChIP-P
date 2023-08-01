1adc5310a33e8882cb0ef55b55b381bfa7a7ad75
Ashley Doerfler <doerflas@oregonstate.edu>
2023-08-01 10:39:11 -0600

    Add most recent code
    There are three different kinds of feature extraction and three different kinds of clustering. Two are commented out for each.

--- bin/ScanChIPP.py
+++ bin/ScanChIPP.py
@@ -4,9 +4,11 @@
 #######################################
 
 from sklearn.cluster import DBSCAN
+from sklearn.cluster import HDBSCAN
 from sklearn.neighbors import NearestNeighbors
 import pandas as pd
 import numpy as np
+from numpy import savetxt
 import seaborn as sns
 from sklearn.metrics import silhouette_score
 from jqmcvi import base
@@ -17,6 +19,7 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 mpl.rcParams['pdf.fonttype']=42
 mpl.rcParams['ps.fonttype']=42
+import re
 
 def main():
     """
@@ -26,27 +29,41 @@ def main():
     parser = setup_parser()
     args = parse_arguments(parser)
     
-    min_points = min_pnts(args.minsize, args.binsize)
+    # min_points = min_pnts(args.minsize, args.binsize)
     
     print("Read data...")
     data = pd.read_csv(args.input, header=None, sep='\t') # remove headers as needed
     
-    data = submatrix(data, 0, 0, 100) # For testing purposes on simulated data from CASPIAN
+    # data = submatrix(data, 0, 0, 100) # For testing purposes on simulated data from CASPIAN
     
-    window_length = len(data) // args.windowproportion
+    data = data.to_numpy()
+    
+    # np.set_printoptions(precision=3, suppress=True)
+    # with open('100x100.txt', 'w') as outfile: # uncomment if using simulated matrix
+    #     np.savetxt(outfile, data)
+    
+    new_matrix = data
+
+    for i in range(0, 30):
+        for j in range(i, 30):
+            new_matrix[i, j] = data[j,i]
+        
+    print(new_matrix)
+    
+    savetxt('sym_30x30.txt', new_matrix, delimiter=' ')
+    
+    window_length = len(new_matrix) // args.windowproportion
     
     print("Create features...")
-    features = create_feature_data(data, window_length)
+    features = create_feature_data(new_matrix, window_length, args.windowproportion)
     
     # Save features to a file
     with open('features2.txt', 'w') as outfile:
         np.savetxt(outfile, features)
-        
-    eps = 29 #find_elbow_point(distances)
     
     print("Find best eps from elbow in range of 10 and cluster...")
     # min_points = min_pnts(args.minsize, args.binsize)
-    best_clusters = cluster_ranges(eps=eps, features=features)
+    best_clusters = cluster_ranges(eps=29, features=features)
     
     tads = generate_tad(bin_size=args.binsize, tad_size=args.minsize, clusters=best_clusters)
     
@@ -65,7 +82,7 @@ def submatrix( matrix, startRow, startCol, size):
 #          Feature Extraction         #
 #######################################
             
-def create_feature_data(matrix, window_length):
+def create_feature_data(matrix, window_length, windowproportion):
     """
     Create a window for feature extraction to exclude a lot of the noise
 
@@ -76,28 +93,53 @@ def create_feature_data(matrix, window_length):
     Returns:
         array: Features, N_features = (2 * window_length) * len(matrix)
     """
+    # Cross Features
     features = []
-    n = 0
-
+    window_length = len(matrix) - windowproportion
+    
     for diag in range(0, len(matrix)):
         feat = []
         # Finds end position of window
-        window = window_length + diag
+        window = window_length // 2
         
-    # Collect row and column features 
-        if diag > len(matrix) / 2:
-            for j in range(n, len(matrix)): 
-            # rows
+    # Collect row and column features
+        # Top-left corner
+        if (diag - window) < 0:
+            for j in range(0, window_length):
+                # row
                 feat.append(matrix[diag, j])
-            for i in range(n, len(matrix)):
-            #      # col
+            for i in range(0, window_length):
+                # col
                 feat.append(matrix[i, diag])
-        else:
-            for j in range(0, len(matrix) - n): 
+        # Bottom-right corner
+        elif (diag + window) > len(matrix):
+            for j in range(len(matrix) - window_length, len(matrix)):
+                feat.append(matrix[diag, j])
+            for i in range(len(matrix) - window_length, len(matrix)):
+                feat.append(matrix[i, diag])
+        # Full cross, and window length is an even nubmer
+        elif window_length % 2 == 0:   
+            for j in range(diag - window, diag + window): 
                 # rows
                 feat.append(matrix[diag, j])
-            for i in range(0, len(matrix) - n):
-            #      # col
+            for i in range(diag - window, diag + window):
+                # col
+                feat.append(matrix[i, diag])
+        # Window is not even and in bottom-right corner
+        elif diag > (len(matrix) - window_length):
+            for j in range((diag - window) - 1, diag + window): 
+                # rows
+                feat.append(matrix[diag, j])
+            for i in range((diag - window) - 1, diag + window):
+                # col
+                feat.append(matrix[i, diag])
+        # Window is not even and in top-left corner
+        else:
+            for j in range(diag - window, (diag + window) + 1): 
+                # rows
+                feat.append(matrix[diag, j])
+            for i in range(diag - window, (diag + window) + 1):
+                # col
                 feat.append(matrix[i, diag])
                 
         # Add the feature to the set of features            
@@ -105,9 +147,71 @@ def create_feature_data(matrix, window_length):
         
     # print(features)
     features = np.array(features, dtype=float)
-    
     print(features)
     return features
+
+    # Square Features
+    # features = []
+    # for diag in range(0, len(matrix)):
+    #     feat = []
+    #     # Finds end position of window
+    #     window = window_length + diag
+        
+    # # Collect row and column features
+
+    #     # If window is out of bounds
+    #     if (window > len(matrix)):
+    #         for i in range(0, window_length): 
+    #             # rows
+    #             for j in range(0, window_length):
+    #                 # col
+    #                 feat.append(matrix[diag - i, diag - j])
+    #     else:   
+    #         for i in range(diag, window): 
+    #             # rows
+    #             for j in range(diag, window):
+    #                 # col
+    #                 feat.append(matrix[i, j])
+    #     # Add the feature to the set of features    
+    #     features.append(feat)
+    
+    # features = np.array(features, dtype=float)
+    
+    # return features
+    
+    # ClusterTAD Features
+    # features = []
+    # n = 0
+
+    # for diag in range(0, len(matrix)):
+    #     feat = []
+    #     # Finds end position of window
+    #     window = window_length + diag
+        
+    # # Collect row and column features 
+    #     # if diag > len(matrix) / 2:
+    #     #     for j in range(n, len(matrix)): 
+    #     #     # rows
+    #     #         feat.append(matrix[diag, j])
+    #     #     for i in range(n, len(matrix)):
+    #     #     #      # col
+    #     #         feat.append(matrix[i, diag])
+    #     # else:
+    #     for j in range(0, len(matrix) - n): 
+    #             # rows
+    #         feat.append(matrix[diag, j])
+    #     for i in range(0, len(matrix) - n):
+    #         #      # col
+    #         feat.append(matrix[i, diag])
+                
+    #     # Add the feature to the set of features            
+    #     features.append(feat)
+        
+    # # print(features)
+    # features = np.array(features, dtype=float)
+    
+    # print(features)
+    # return features
 
 #######################################
 #          DBScan Clustering          #
@@ -175,22 +279,22 @@ def cluster_ranges(eps, features):
         min_points (int): DBSCAN minimum neighbors
         features (2D array): Dataset for Clustering
     """
+    
     best_score_s = 0
-    best_score_dunn = 0
     best_file = ""
-    best_file_dunn = ""
     best_cluster = []
-    for e in range(1, eps+100):
+    for e in range(1, 10):
         
         # Cluster TADs
-        clusters = DBSCAN(eps=e, min_samples=3).fit(features)
+        # clusters = DBSCAN(eps=e, min_samples=3).fit(features)
+        # clusters = HDBSCAN().fit(features)
+        clusters = KMeans(n_clusters=e, random_state=0, n_init="auto").fit(features)
         
         # Count the number of different labels
         labels = clusters.labels_
         print(labels)
         n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-        n_noise_ = list(labels).count(-1)
-
+            
         if n_clusters_ <= 1:
             continue
             
@@ -312,11 +416,30 @@ def readTAD(tadfile):
         line = f.readline()
     f.close()
     return start, end
+# def readTAD(tadfile):
+#     f = open(tadfile)
+#     line = f.readline()
+#     start = []
+#     end = []
+#     while line:
+#         line = line.split()
+#         try:
+#             start1 = int(float(line[0]))
+#         except:
+#             start1 = 0
+#         end1 = int(float(line[1]))
+#         start.append(start1)
+#         end.append(end1)
+#         line = f.readline()
+#     f.close()
+
+#     # Split the start and end lists into separate lists
+#     start = start[::2]  # Start from the beginning and take every second element
+#     end = end[1::2]     # Start from the second element and take every second element
+
+#     return start, end
 
 def create_heatmap(hic, tadfile):
-    
-    # Metric list for TAD identification
-    metricList = []
 
     # Read TAD boundaries using the readTAD function
     start, end = readTAD(tadfile)
@@ -328,15 +451,19 @@ def create_heatmap(hic, tadfile):
     # Plot the Hi-C matrix heatmap and highlight TAD boundaries
     palette = sns.color_palette("bright", 10)
     plt.figure(figsize=(10.5, 10))
-    start1 = 140
-    end1 = 250
-    sns.heatmap(data=hic[start1:end1, start1:end1], robust=True, cmap="OrRd")
+    start1 = 0
+    end1 = 7
+    try:
+        sns.heatmap(data=hic, robust=True, cmap="OrRd")
+    except UserWarning:
+        sns.heatmap(data=hic[start1:end1, start1:end1], robust=True, cmap="OrRd", vmin=0, vmax=1)
+
     for i in range(0, lentad):
-        if start1 < start[i] < end1 and start1 < end[i] < end1:
+        if start1 < start[i] < end1 and start1 < end[i]:
             plt.hlines(y=start[i] - start1, xmin=start[i] - start1, xmax=end[i] - start1)
             plt.vlines(x=end[i] - start1, ymin=start[i] - start1, ymax=end[i] - start1)
     plt.title('TAD boundary')
-    plt.savefig('../example/{}_{}_{}.pdf'.format(start1, end1), format='pdf', bbox_inches='tight')
+    plt.savefig('../example/{}_{}_{}.pdf'.format(start1, end1, ''), format='pdf', bbox_inches='tight')
     plt.show()
 
 #######################################
@@ -359,6 +486,8 @@ def parse_arguments(parser):
     return args
 
 main()
+
+
 
 
 
